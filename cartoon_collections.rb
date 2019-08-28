e6cf147372e54cb6cd6c018fc45e5cecb076889a
Ali Shah <ali1195shah@gmail.com>
2019-08-28 02:38:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-  
+def roll_call_dwarves(arr)
+  arr.each_with_index { |num, idx| puts "#{idx + 1} #{num}"}
 end
 
 def summon_captain_planet# code an argument here
